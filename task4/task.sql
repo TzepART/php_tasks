e52e99135228dcfb71aedd7da8f3a58f35ae3538
tzepart <tzepart@gmail.com>
2018-06-03 02:15:29 +0300

    w_task4_add_correct_variant

--- task4/task.sql
+++ task4/task.sql
@@ -1,4 +1,6 @@
 SELECT id INTO @prev_id FROM library_data_base.test ORDER BY test.id limit 1;
-SELECT id INTO @prev_id FROM library_data_base.test WHERE (id - @prev_id) > 1 ORDER BY test.id limit 1;
-SELECT @prev_id as 'FROM', @prev_id := id as 'TO'  FROM library_data_base.test as test
-WHERE (id - @prev_id) > 1 ORDER BY test.id;
+SELECT temp.prev_id as 'FROM', temp.current_id as 'TO' FROM
+	(SELECT (id - @prev_id) as diff, @prev_id as prev_id, @prev_id := id as current_id
+	FROM library_data_base.test as test
+	ORDER BY test.id) as temp
+WHERE temp.diff >1;
