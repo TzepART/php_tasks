0a4f66197cc17c9dc016c0d2c4b3cac533b6235d
tzepart <tzepart@gmail.com>
2018-06-03 20:43:36 +0300

    w_task4_version_2

--- task4/task.php
+++ task4/task.php
@@ -6,42 +6,64 @@ function withBcadd ($a, $b) {
 }
 
 function myFunction ($a, $b){
-    $result = '';
+    /*
+     * Проверить дробные-ли они или нет
+     * если да - разделить на дробную и целую части
+     * сложить два масива
+     * реверсим их
+     * Если 1-ый больше, то цикл по-нему, если нет, то по первому
+     * бежим по результирующему циклу, если > 10, следующий увеличиваем на 1 а от числа отбрасываем 10
+     */
     $arNumbersA = array_reverse(str_split($a));
     $arNumbersB = array_reverse(str_split($b));
 
     $sizeA = count($arNumbersA);
     $sizeB = count($arNumbersB);
-    $temResult = [];
 
-    foreach ($arNumbersA as $index => $numberA) {
-        $tempSumm = $numberA + $arNumbersB[$index];
-        if($tempSumm >= 10){
+    if($sizeA > $sizeB){
+        $largeNumberAr = $arNumbersA;
+        $smallNumberAr = $arNumbersB;
+        $arSize = $sizeA;
+    }else{
+        $largeNumberAr = $arNumbersB;
+        $smallNumberAr = $arNumbersA;
+        $arSize = $sizeB;
+    }
+
+    $resultReversAr = [];
+    foreach ($largeNumberAr as $index => $number) {
+        $resultReversAr[] = $number + (isset($smallNumberAr[$index]) ? $smallNumberAr[$index] : 0);
+    }
+
+    for($i = 0; $i < $arSize; $i++) {
+        $item = $resultReversAr[$i];
+        if($item >= 10){
             $diff = 1;
-            $tempResultValue = $tempSumm - 10;
+            $tempResultValue = $item - 10;
         }else{
             $diff = 0;
-            $tempResultValue = $tempSumm;
+            $tempResultValue = $item;
         }
 
-        if(isset($arNumbersB[$index+1])){
-            $arNumbersB[$index+1] = $arNumbersB[$index+1] + $diff;
-            $temResult[] = $tempResultValue;
-        }else{
-            $temResult[] = $tempResultValue;
-            if($diff == 1){
-                $temResult[] = 1;
+        $resultReversAr[$i] = $tempResultValue;
+
+        if($diff == 1){
+            if(isset($resultReversAr[$i+1])){
+                $resultReversAr[$i+1] = $resultReversAr[$i+1] + $diff;
+            }else{
+                $resultReversAr[] = $diff;
             }
         }
     }
-    $resultAr = array_reverse($temResult);
+
+    $resultAr = array_reverse($resultReversAr);
     $result = implode('',$resultAr);
 
     echo "witmyFunc - ".$result."\n";
 }
 
 
-$a = "53503534226472524250874054075591789781264330331690";
+$a = "5350353422647252425087405404345435456456456456456456755917897812643";
 $b = "53503534226472524250874054075591789781264330331690";
 
 withBcadd($a, $b);
