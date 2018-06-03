91e040419378df36ff2b2d06fcee6e9738fc7a13
tzepart <tzepart@gmail.com>
2018-06-04 02:08:32 +0300

    w_task2_fix

--- task2/test.php
+++ task2/test.php
@@ -2,10 +2,9 @@
 /*
  * 1. Использовать полный путь до файла, а не относительный
  * 2. Должна быть проверка на существование файла
- * 3. Должна быть проверка на возможность чтения файла
- * 4. Должна быть проверка на возможность записи в файл
- * 5. Использовать блокировку файла на время чтения и записи
- * 6. Использовать приведение типов к int, если файл содержит например текст
+ * 3. Должна быть проверка на возможность чтения и записи в файл
+ * 4. Использовать блокировку файла на время чтения и записи
+ * 5. Использовать приведение типов к int, если файл содержит например текст
  * Проблема при параллельном использовании этого счетчика
  * */
 
@@ -14,39 +13,25 @@ $logPath = __DIR__."/log.txt"; // Full Path
 $logMessage = '';
 
 if(file_exists($fullPath)){
-    $fpRead = fopen($fullPath, "r");
-    // проверка файла на чтение
-    if ($fpRead) {
-        $locked = flock($fpRead, LOCK_SH, $waitIfLocked); // Блокирование файла для чтения
+    $fp = fopen($fullPath,"a+");
+    // проверка файла на чтение и на запись
+    if ($fp) {
+        $locked = flock($fp,LOCK_EX); // Блокирование файла для чтения и записи
         if($locked) {
-            $fileSize = filesize($fullPath) > 0 ? filesize($fullPath) : 80;
-            $count = (int) fread($fpRead, $fileSize);
-            flock($fpRead, LOCK_UN); // Снятие блокировки
-            fclose($fpRead);
-
-            $fpWrite = fopen($fullPath, "w");
-            $locked = flock($fpWrite, LOCK_EX, $waitIfLocked); // Блокирование файла для записи
-            // проверка файла на запись
-            if($fpWrite){
-                if($locked) {
-                    fwrite($fpWrite, $count + 1);
-                    fflush($fpWrite);
-                    flock($fpWrite, LOCK_UN); // Снятие блокировки
-                } else {
-                    $logMessage .= "File blocked on write! Status - $waitIfLocked\n";
-                }
-                fclose($fpWrite);
-            }else{
-                $logMessage .= "The file is not writable\n";
-                fclose($fpWrite);
-            }
+            $fileSize = filesize($fullPath) > 0 ? filesize($fullPath) : 100;
+            $count=fread($fp,$fileSize);
+            @$count++;
+            ftruncate($fp,0);//предварительно очистим файл
+            fwrite($fp,$count);
+            fflush($fp);
+            flock($fp,LOCK_UN);
+            fclose($fp);
         }else{
-            fclose($fpRead);
-            $logMessage .= "File blocked on read! Status - $waitIfLocked !\n";
+            $logMessage .= "File blocked on read and write!\n";
+            fclose($fp);
         }
     } else {
-        $logMessage .= "The file is not readable\n";
-        fclose($fpRead);
+        $logMessage .= "The file is not available\n";
     }
 }else{
     file_put_contents($fullPath, 1);
@@ -54,5 +39,5 @@ if(file_exists($fullPath)){
 
 //логирование ошибок
 if(!empty($logMessage)){
-    file_put_contents($logPath, $logMessage);
+    file_put_contents($logPath, $logMessage, FILE_APPEND);
 }
