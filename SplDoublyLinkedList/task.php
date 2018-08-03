0b2c969f516084d8bc311bd0868da0cbe2529191
tzepart <tzepart@gmail.com>
2018-08-03 11:45:42 +0300

    w_upd_methods_description

--- SplDoublyLinkedList/task.php
+++ SplDoublyLinkedList/task.php
@@ -7,30 +7,31 @@
  */
 
 /*
- * Придумать примеры использующие методы класса SplDoublyLinkedList
- * add
- * bottom
- * count
- * current
- * getIteratorMode
- * isEmpty
- * key
- * next
- * offsetExists
- * offsetGet
- * offsetSet
- * offsetUnset
- * pop
- * prev
- * push
- * rewind
- * serialize
- * setIteratorMode
- * shift
- * top
- * unserialize
- * unshift
- * valid
+ * SplDoublyLinkedList methods (http://php.net/manual/en/class.spldoublylinkedlist.php):
+ *
+ * add - Add/insert a new value at the specified index
+ * bottom - Peeks at the node from the beginning of the doubly linked list
+ * count - Counts the number of elements in the doubly linked list
+ * current - Return current array entry
+ * getIteratorMode - Returns the mode of iteration (IT_MODE_LIFO,IT_MODE_FIFO,IT_MODE_DELETE,IT_MODE_KEEP)
+ * isEmpty - Checks whether the doubly linked list is empty
+ * key - Return current node index
+ * next - Move to next entry
+ * offsetExists($index) - Returns whether the requested $index exists
+ * offsetGet($index) - Returns the value at the specified $index
+ * offsetSet($index,$newval) - Sets the value at the specified $index to $newval
+ * offsetUnset($index) - Unsets the value at the specified $index
+ * pop - Pops a node from the end of the doubly linked list (end)
+ * prev - Move to previous entry
+ * push - Pushes an element at the end of the doubly linked list
+ * rewind - Rewind iterator back to the start
+ * serialize - Serializes the storage
+ * setIteratorMode(int $mode) - Sets the mode of iteration (IT_MODE_LIFO,IT_MODE_FIFO,IT_MODE_DELETE,IT_MODE_KEEP)
+ * shift - Shifts a node from the beginning of the doubly linked list
+ * top - Peeks at the node from the end of the doubly linked list
+ * unserialize - Unserializes the storage
+ * unshift - Prepends the doubly linked list with an element
+ * valid - Check whether the doubly linked list contains more nodes
  */
 
 /**
